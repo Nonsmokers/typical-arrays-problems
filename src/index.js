d719046371ed2ec27353cc188f3435913e21decf
Nonsmokers <Vasyuxnevichs@mail.ru>
2020-03-12 10:04:49 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array.length == 0) return 0;
+    return value = Math.min.apply(null, array)
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length == 0) return 0;
+    return value = Math.max.apply(null, array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array.length == 0) return 0;
+    let sum = 0;
+    for(var i = 0; i < array.length; i++){
+        sum += array[i];
+        }
+        return sum/array.length
 }
